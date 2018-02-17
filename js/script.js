9c00abd37db80cb3859a643759d59104de652997
Ryan Yang <javathon@gmail.com>
2018-02-17 14:14:59 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,63 @@
+window.onload = function () {
+	MIDI.loadPlugin({
+		soundfontUrl: "./soundfont/",
+		instrument: "acoustic_grand_piano",
+		onprogress: function(state, progress) {
+			console.log(state, progress);
+		},
+		onsuccess: function() {
+            playMusic();
+		}
+	});
+};
+
+function pN(note, delay){
+    var num = 0;
+    var vel = 127;
+    MIDI.setVolume(num, 127);
+    MIDI.noteOn(num, note, vel, delay);
+    MIDI.noteOff(num, note, delay + 0.75);
+}
+
+function playMusic(){
+    //var bass = setInterval(bassLine, 8000);
+    bassLine();
+    //pN(48, 1);
+    //pN(50, 1.5);   
+}
+
+function bassLine(octave){
+    
+    
+    
+}
+
+function gC(type, note, delay){
+    switch(type){
+        case 'maj':
+            pN(note, delay);
+            pN(note+4, delay);
+            pN(note+7, delay);
+            break;
+        case 'min':
+            pN(note, delay);
+            pN(note+3, delay);
+            pN(note+7, delay);
+            break;
+        case 'dim':
+            break;
+        case 'aug':
+            break;
+        default:
+            console.log("chord type not recognized");
+            break;
+    }
+}
+function pF(note, delay){
+    pN(note, delay);
+    pN(note+7, delay);
+}
+function pO(note, delay){
+    pN(note, delay);
+    pN(note+12, delay);
+}
