17a7a8f3051e6cd3ace75c63c4fc87684cdb3c6e
Ryan Yang <javathon@gmail.com>
2018-02-17 15:50:30 -0800

    added chords function and bpmso if bpm and chord progression is set, it can play the basic chord progression

--- js/script.js
+++ js/script.js
@@ -11,19 +11,28 @@ window.onload = function () {
 	});
 };
 
+var bps = 1;
+
 function pN(note, delay){
     var num = 0;
     var vel = 127;
     MIDI.setVolume(num, 127);
     MIDI.noteOn(num, note, vel, delay);
-    MIDI.noteOff(num, note, delay + 0.75);
+    MIDI.noteOff(num, note, delay + 1);
 }
 
 function playMusic(){
-    //var bass = setInterval(bassLine, 8000);
-    bassLine();
+    setBPM(102);
+    var bass = setInterval(chords, bps*8000);
+    //chords();
+   
+    //chords(3);
     //pN(48, 1);
     //pN(50, 1.5);   
+}
+function setBPM(bpm){
+    bps = (bpm/60);
+    bps = parseFloat(1.0/bps);
 }
 
 function bassLine(octave){
@@ -31,8 +40,11 @@ function bassLine(octave){
     
     
 }
-function chords(octave){
-    
+function chords(){
+    gC('min', 57, 0);
+    gC('maj', 55, bps*2);
+    gC('maj', 48, bps*4);
+    gC('maj', 48, bps*6);
 }
 
 function gC(type, note, delay){
@@ -48,6 +60,9 @@ function gC(type, note, delay){
             pN(note+7, delay);
             break;
         case 'dim':
+            pN(note, delay);
+            pN(note+3, delay);
+            pN(note+6, delay);
             break;
         case 'aug':
             break;
